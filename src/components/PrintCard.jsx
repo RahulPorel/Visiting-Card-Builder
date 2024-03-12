b354f6a58d46011920ac9da203f64c19be2ad69c
rahulporel51 <rahulporel51@gmail.com>
2024-03-12 15:51:20 +0530

    add package

--- src/components/PrintCard.jsx
+++ src/components/PrintCard.jsx
@@ -2,7 +2,7 @@ export default function PrintCard() {
   const Print = () => {
     //console.log('print');
     let printContents = document.getElementById("center-container").innerHTML;
-    let originalContents = document.body.innerHTML;
+    let `originalContents` = document.body.innerHTML;
     document.body.innerHTML = printContents;
     window.print();
     document.body.innerHTML = originalContents;
