f0deaf841cf36f4465e1e52c394a09cf0d60bc33
rahulporel51 <rahulporel51@gmail.com>
2024-03-12 18:50:36 +0530

    rm img

--- src/components/staticData.js
+++ src/components/staticData.js
@@ -1,6 +1,5 @@
 const staticData = {
   perosnalInfo: {
-    img: "./assests/static_assests/20240226_192140.jpg",
     name: "Rahul Porel",
     proffession: "Frontend Developer",
     email: "rahulporel51@gmail.com",
