8ff6b576a5c5a004a6ef62353356a61e66503c5e
rahulporel51 <rahulporel51@gmail.com>
2024-03-14 16:23:08 +0530

    bug fixex

--- src/components/EditBtn.jsx
+++ src/components/EditBtn.jsx
@@ -6,11 +6,15 @@ function EditBtn(props) {
   };
 
   return (
-    <div className="button" id="button-3" onClick={handleEditBtn}>
+    <div
+      className="button button-for-mobile"
+      id="button-3"
+      onClick={handleEditBtn}
+    >
       <div id="circle"></div>
       <button className="edit-card-btn rm-default-btn-styles mobile-styling-copy-btn">
         <i className="fa-regular fa-pen-to-square"></i>
-        Edit Card
+        &nbsp; Edit Card
       </button>
     </div>
   );
