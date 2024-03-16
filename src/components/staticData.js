ecfc60b860a94870ee8923f5cd3f88769d529385
rahulporel51 <rahulporel51@gmail.com>
2024-03-16 17:15:45 +0530

    code update

--- src/components/staticData.js
+++ src/components/staticData.js
@@ -1,13 +1,19 @@
 // staticDb that will show on every page load by default
-import { randEmail, randFullName, randPhoneNumber, randJobTitle } from "@ngneat/falso";
+import {
+  randEmail,
+  randFullName,
+  randPhoneNumber,
+  randJobTitle,
+  randUrl,
+} from "@ngneat/falso";
 const staticData = {
   perosnalInfo: {
-    // name: "Rahul Porel",
-    proffession: "Frontend Developer",
-    email: randEmail(),
-    phone_no: "9999-9999-01",
-    website: "www.rahulporel.com",
     name: randFullName(),
+
+    proffession: randJobTitle(),
+    email: randEmail(),
+    phone_no: randPhoneNumber(),
+    website: randUrl(),
   },
 };
 export default staticData;
